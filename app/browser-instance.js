f8a70414f3338dccface06442b15c2772c359988
Franklin Davenport <sparebytes@gmail.com>
2019-09-03 03:25:24 +0000

    fix: LAUNCH_HC_PAGES_NUM is honored again. A buffer of pages for later use will be created

--- app/browser-instance.js
+++ app/browser-instance.js
@@ -26,9 +26,8 @@ module.exports.getBrowserInstance = async () => {
     throw msg
   })
 
-  const usePage = async (callback) => {
-    let page
-    let context = null
+  const createPageContext = async () => {
+    let context, page
     if (appConfig.useIncognitoBrowserContext) {
       context = await browser.createIncognitoBrowserContext()
       page = await context.newPage()
@@ -39,7 +38,44 @@ module.exports.getBrowserInstance = async () => {
     page.on('error', msg => {
       throw msg
     })
+    return { page, context }
+  }
+
+  const awaitingPageContextDefferds = []
+  const availablePageContexts = []
   
+  const readyNextPageContext = async () => {
+    const pageContext = await createPageContext()
+    pushNextPageContext(pageContext)
+  }
+
+  const pushNextPageContext = pageContext => {
+    const awaitingPageContextDeferred = awaitingPageContextDefferds.shift()
+    if (awaitingPageContextDeferred != null) {
+      awaitingPageContextDeferred.resolve(pageContext)
+    } else {
+      availablePageContexts.push(pageContext)
+    }
+  }
+
+  if (appConfig.launchHcPagesNum > 0) {
+    for (let i = 0; i < appConfig.launchHcPagesNum; i++) {
+      readyNextPageContext()
+    }
+  }
+  else {
+    throw new Error('LAUNCH_HC_PAGES_NUM must be greater than 0')
+  }
+
+  const usePage = async (callback) => {
+    let pageContext = availablePageContexts.shift()
+
+    if (pageContext == null) {
+      pageContext = await new Promise((resolve, reject) => awaitingPageContextDefferds.push({resolve, reject}))
+    }
+
+    const { page, context } = pageContext
+
     try {
       const result = await callback(page)
       return result
@@ -51,9 +87,9 @@ module.exports.getBrowserInstance = async () => {
       else {
         page.close()
       }
+      readyNextPageContext()
     }
   }
-  
 
   return {
     puppeteer,
